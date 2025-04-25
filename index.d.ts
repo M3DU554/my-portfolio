502f47c658d2830b547aee1ad9a23b2fca6ebd83
M3DU554 <karen14marrufo@gmail.com>
2025-04-24 20:22:40 -0500

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,2 @@
-import { trace, exportTraceState, flushAllTraces, getTraceEvents, initializeTraceState, recordTraceEvents, Span, SpanStatus } from './trace';
-import { setGlobal } from './shared';
-import type { SpanId, TraceEvent, TraceState } from './types';
-export { trace, exportTraceState, flushAllTraces, getTraceEvents, initializeTraceState, recordTraceEvents, Span, setGlobal, SpanStatus, };
-export type { SpanId, TraceEvent, TraceState };
+export { getSortedRoutes, getSortedRouteObjects } from './sorted-routes';
+export { isDynamicRoute } from './is-dynamic';
